89a81a2ad55a098ce4d30aceefd8f4abab1d925e
Paulade91 <paulannadebby@yahoo.com>
2025-08-03 05:06:46 +0000

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { Route, Routes } from "react-router-dom";
+import CategoryCard from "./components/CategoryCard";
+import UserManual from "./components/UserManual";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
-      {/* <Route  path="/" element={<Home />} /> */}
+      <Route
+        path="/"
+        element={
+          <div>
+            <CategoryCard />
+            <UserManual />
+          </div>
+        }
+      />
+      {/* Add more routes here as needed */}
     </Routes>
   );
 }
