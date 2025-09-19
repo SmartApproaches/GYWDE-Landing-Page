6f28a4cc654e9e50431ff389dc86d9101ccf7b59
Kemi Oladipupo <seyiolukoya@gmail.com>
2025-09-19 13:24:22 +0100

    fix: margin and spacing
    refactor: added the nav and herosection to home

--- src/constants/images/index.js
+++ src/constants/images/index.js
@@ -1,5 +1,21 @@
+export {default as logo} from "../images/logo.svg"
+export {default as globe} from "../images/globe.svg"
 export {default as RingOne} from "../images/RingOne.svg"
 export {default as RingTwo} from "../images/RingTwo.svg"
+
+// //GYWDE-HOME SECTION Images
+// export {default as applePlay} from "../images/applePlay.svg";
+// export {default as googlePlay} from "../images/googlePlay.svg";
+// export {default as plainPhone} from "../images/plainPhone.svg";
+// export {default as splashScreenPhone} from "../images/splashScreenPhone.svg";
+// export {default as signUpPhone} from "../images/signUpPhone.svg";
+// export {default as exploreServicesPhone} from "../images/exploreServicesPhone.svg";
+// export {default as perfectMatchPhone} from "../images/perfectMatchPhone.svg";
+// export {default as successPhone} from "../images/successPhone.svg";
+
+// // // ABOUT US SECTION Images
+// export {default as worldMap} from "../images/worldMap.svg";
+// export {default as videoThumbNail} from "../images/videoThumbNail.svg"
 
 // whyChooseUs Images
 export {default as securePayment} from "../images/securePayment.svg";
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,25 @@
 import { RingOne, RingTwo } from "../constants/images";
-import WhyChooseUs from "../components/WhyChooseUs";
+import NavBar from "../components/NavBar";
+import HeroSection from "../components/HeroSection";
 
+import WhyChooseUs from "../components/WhyChooseUs";
 
 function Home() {
   return (
     <>
-      <section className="relative  bg-[#E6F5F9] overflow-hidden ">
+      <section className=" bg-[var(--background)]  min-h-screen overflow-hidden relative">
+        <div className="absolute top-[270px] md:top-[200px] -left-[2px] z-0 md:size-40 size-30">
+          <img src={RingOne} />
+        </div>
+        <div className="absolute -top-[20px] right-0 z-0 md:size-85 size-60">
+          <img src={RingTwo} />
+        </div>
+
+        <NavBar />
+        <HeroSection />
+        </section>
+
+        <section className="relative  bg-[#E6F5F9] overflow-hidden">
         <div className="absolute hidden lg:block -bottom-5 -left-5 z-0 size  md:size-60 lg:size-70 rotate-180 ">
           <img src={RingTwo} />{" "}
         </div>
@@ -15,6 +29,7 @@ function Home() {
 
         <WhyChooseUs />
       </section>
+      
     </>
   );
 }
