aaf9c2f8f89171feb97d3eb94512f049fbe4f19c
Kemi Oladipupo <seyiolukoya@gmail.com>
2025-09-19 13:28:18 +0100

    fix: spacing

--- src/components/WhyChooseUs.jsx
+++ src/components/WhyChooseUs.jsx
@@ -38,23 +38,23 @@ function WhyChooseUs() {
   return (
     <>
       <div className=" max-w-6xl mx-auto my-15 flex flex-col lg:flex-row justify-between gap-2 bg-[var(--background-secondary)]] ">
-        <div className="mx-auto text-center lg:text-start mx-4 md:mx-8 gap-10 ">
+        <div className=" text-center lg:text-start mx-4 md:mx-12 gap-10 ">
           <h1 className="uppercase text-[22.69px] lg:text-[40px] leading-[1.5] text-[var(--text-other)] font-semibold text-center lg:text-start ">
             _why choose us_
           </h1>
-          <p className="font-bold text-[31.19px] mt-[22px] lg:text-[55px] text-[var(--primary)] lg:leading-[1.5]">
+          <p className="font-bold text-[28.19px] mt-[22px] lg:text-[55px] text-[var(--primary)] lg:leading-[1.5]">
             Elevate your 
             <br className="hidden  lg:block" /> Service Experience
           </p>
         </div>
-        <div className="cards flex flex-col lg:flex-row mx-6 gap-4 lg:gap-0 ">
+        <div className="cards flex flex-col lg:flex-row mx-10 gap-4 lg:gap-0 lg:-mr-15">
           {cards.map((card, key) => {
             const isLast = key === cards.length - 4;
 
             return (
               <div
                 key={key}
-                className={`relative border border-3 border-[var(--primary)] px-4 py-10 lg:py-20 mt-4 lg:px-4  rounded-3xl lg:rounded-[50px] 
+                className={`relative border border-1 lg:border-3 border-[var(--primary)] sm:mx-4 lg:mx-0 px-4 py-10 lg:py-20 mt-4 lg:mt-0 lg:px-5  rounded-3xl lg:rounded-[50px] 
                 ${
                   isLast
                     ? "bg-[var(--lighter-bg)] text-[var(--text-secondary)] z-0"
@@ -64,13 +64,14 @@ function WhyChooseUs() {
               >
                 <img src={card.icon} className="size-12 lg:size-16 mx-auto" />
                 <div className="text-center justify-center place-self-center mt-2 lg:mt-5 ">
-                  <h1 className="text-xl lg:text-[36.06px] mt-5 text-[var(--secondary)] font-bold whitespace-nowrap text-center">
+                  <h1 className="text-xl lg:text-[32.06px] mt-5 text-[var(--secondary)] font-bold whitespace-nowrap text-center">
                     {card.title}
                   </h1>
                   <p className="mt-5 text-lg lg:text-[30px] text-center text-justify lg:w-sm mx-auto lg:mx-0 font-medium">
                     {card.description}
                   </p>
                 </div>
+                
               </div>
             );
           })}
