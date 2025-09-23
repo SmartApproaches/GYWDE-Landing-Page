4876b5f873f07b32c1761b88acf6ae512d2859a4
Paulade91 <paulannadebby@yahoo.com>
2025-09-23 11:18:47 +0000

    Adjusted the stylings

--- src/pages/About-Us/constants/images/index.js
+++ src/pages/About-Us/constants/images/index.js
@@ -4,12 +4,15 @@ export { default as mapbase } from "./mapbase.svg";
 export { default as frame1651 } from "./Frame1651.svg";
 export { default as frame1617 } from "./Frame1617.svg";
 
-//Quote icon
+//What set us apart icons
 export { default as oui_quote} from "./oui_quote.svg";
 export { default as mdi_arm_flex_outline } from "./mdi_arm_flex_outline.svg";
 export { default as ic_baseline_gpp_good  } from "./ic_baseline_gpp_good.svg";
 export { default as fluent_diversity_20_filled  } from "./fluent_diversity_20_filled.svg";
 export { default as tdesign_money  } from "./tdesign_money.svg";
+export {default as EllipseDown} from "./EllipseDown.svg"
+export {default as EllipseUp} from "./EllipseUp.svg"
 
 //FAQ image
 export { default as faqImage } from "./faqImage.svg";
+
